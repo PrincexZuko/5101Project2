5fb500e63ed24344718b484b817a5f5fa53f4c01
Princeton Gill <PrincetonGill@hotmail.com>
2025-04-08 09:41:15 -0400

    Add project files.

---
+++ 5101Project2/JsonFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace _5101Project2
+{
+    public class JsonFile
+    {
+        public static List<InfixExpression> ParseJson(string filePath)
+        {
+            // Check if the file exists
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File not found: {filePath}");
+            }
+            // Read all text from the file
+            string jsonContent = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<InfixExpression>>(jsonContent);
+        }
+    }
+
+    // class structure for JSON data
+    public class InfixExpression
+    {
+        public int Sno { get; set; } 
+        public string Infix { get; set; } 
+    }
+}
---
+++ 5101Project2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5101Project2
+{
+    class Program{   
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filePath = @"Data\infix-expressions.json";
+                List<InfixExpression> expressions = JsonFile.ParseJson(filePath);
+
+                foreach (var expression in expressions)
+                {
+                    Console.WriteLine($"Sno: {expression.Sno}, Infix: {expression.Infix}"); //test
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
