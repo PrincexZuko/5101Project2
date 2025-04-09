423294e206bef44fd4fef5b324d62aa70b1bbced
Princeton Gill <PrincetonGill@hotmail.com>
2025-04-09 11:58:51 -0400

    added code to the CompareExpressions

--- 5101Project2/CompareExpressions.cs
+++ 5101Project2/CompareExpressions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _5101Project2._5101Project2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace _5101Project2
 {
-    public class CompareExpressions
+    /*
+     * Class Name: CompareExpressions
+     * Purpose: Compare prefix and postfix evaluation results.
+     * Methods: Compare()
+     * Coder: KG
+     * Date: April 8, 2025
+     */
+    public class CompareExpressions : IComparer<EvaluationResult>
     {
+        /*
+         * Method name: Compare()
+         * Purpose: Compare prefix and postfix evaluation results for a given expression.
+         * Accepts: EvaluationResult (x, y)
+         * Returns: int
+         * Coder: KG
+         * Date: April 9, 2025
+         */
+        public int Compare(EvaluationResult x, EvaluationResult y)
+        {
+            if (x == null || y == null)
+                throw new ArgumentNullException("One or both objects to compare are null.");
+
+            if (Math.Abs(x.PreFixRes - x.PostFixRes) < 0.0001)
+            {
+                return 0;             
+            }
+            return x.PreFixRes > x.PostFixRes ? 1 : -1;
+        }
     }
 }
---
+++ 5101Project2/EvaluationResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5101Project2
+{
+    namespace _5101Project2
+    {
+        /*
+         * Class Name: EvaluationResult
+         * Purpose: Store processed data for an arithmetic expression, including conversions and evaluations.
+         * Properties: Sno, InFix, PreFix, PostFix, PreFixRes, PostFixRes, Match (bool)
+         * Coder: KG
+         * Date: April 9, 2025
+         */
+        public class EvaluationResult
+        {
+            public int Sno { get; set; }
+            public string InFix { get; set; }
+            public string PreFix { get; set; }
+            public string PostFix { get; set; }
+            public double PreFixRes { get; set; }
+            public double PostFixRes { get; set; }
+            public bool Match { get; set; }
+        }
+    }
+}
